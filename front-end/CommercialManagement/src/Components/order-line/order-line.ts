dfda573d328a8e9c34f12310b9fa8fb740ff8cea
Malek Aiter <malek.aiter03@gmail.com>
2026-07-08 18:05:29 +0100

    Setting up Angular core and components

---
+++ front-end/CommercialManagement/src/Components/order-line/order-line.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-line',
+  imports: [],
+  templateUrl: './order-line.html',
+  styleUrl: './order-line.css',
+})
+export class OrderLine {}
